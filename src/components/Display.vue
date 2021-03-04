b573e9330b92aa5c4ab8775a77d372b5de341813
payal-12-3 <payaldholakiya312@gmail.com>
2021-03-04 16:51:08 +0530

    first commit

---
+++ src/components/Display.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="display">
+    <div class="data" v-show="detail.length" v-for="(data, index) in detail">
+      <h4>{{ data.type }}</h4>
+      <p>
+        {{ data.form }} is owed $ {{ data.amount }} for {{ data.details }} .
+        <button @click="deleteItem(index)">delete</button>
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+import Form from './Form'
+export default {
+  name: 'Display',
+  props: {
+    detail: {
+      type: Array,
+      default: null,
+    },
+  },
+  components: {
+    Form,
+  },
+  methods: {
+    deleteItem(index) {
+      this.detail.splice(index, 1)
+    },
+  },
+}
+</script>
+<style scoped>
+h1 {
+  color: fuchsia;
+}
+.display {
+  height: 40vh;
+  width: 100%;
+  overflow-y: scroll;
+}
+.data {
+  margin-left: 20%;
+  margin-right: 20%;
+  margin-top: 5px;
+  padding-left: 5px;
+  border: 1px solid gainsboro;
+  border-radius: 5px;
+}
+h4 {
+  text-align: start;
+  color: fuchsia;
+}
+p {
+  text-align: start;
+}
+p button {
+  text-align: end;
+}
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,95 @@
+<template>
+  <h1>Finance Logger</h1>
+  <Display :detail="arrayData" />
+  <div class="form-data">
+    <div>
+      Type:
+      <select id="my_select" v-model="formData.type">
+        <option value="invoice" id="invoice">invoice</option>
+        <option value="payment" id="payment">payment</option>
+      </select>
+    </div>
+    <div style="padding-top:2px">
+      To/Form:
+      <input v-model="formData.form" type="text" class="input-data" id="form" />
+    </div>
+    <div style="padding-top:2px">
+      Details:
+      <input
+        v-model="formData.details"
+        type="text"
+        class="input-data"
+        id="details"
+      />
+    </div>
+    <div style="padding-top:2px">
+      Amount:
+      <input
+        v-model="formData.amount"
+        type="text"
+        class="input-data"
+        id="amount"
+      />
+    </div>
+    <div class="add">
+      <button @click="add()" type="submit" value="Add" id="add">
+        Add
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+import Display from './Display'
+
+export default {
+  components: {
+    Display,
+  },
+  name: 'Form',
+  data() {
+    return {
+      arrayData: [],
+      formData: {
+        type: 'invoice',
+        form: '',
+        details: '',
+        amount: '',
+      },
+    }
+  },
+  methods: {
+    add() {
+      {
+        this.arrayData.push({ ...this.formData })
+        this.formData.type = ''
+        this.formData.form = ''
+        this.formData.details = ''
+        this.formData.amount = ''
+      }
+    },
+  },
+}
+</script>
+<style scoped>
+h1 {
+  color: fuchsia;
+}
+.form-data {
+  padding-top: 3px;
+  background-color: gainsboro;
+  border: 1px solid gray;
+  border-radius: 5px;
+  margin-left: 10%;
+  margin-right: 10%;
+}
+
+.add button {
+  width: 90px;
+  background-color: magenta;
+  border-radius: 5px;
+}
+.add {
+  padding-top: 2px;
+  margin-left: 5%;
+}
+</style>
